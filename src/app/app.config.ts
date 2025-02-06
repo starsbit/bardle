d3fad3f714911ae630c5869cbca37fbb1792583a
Nico Muszalczyk <74101214+starsbit@users.noreply.github.com>
2025-02-06 18:33:25 +0100

    fix deployment errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch()), provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay()),
   ],
 };
