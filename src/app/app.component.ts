3387f294e104cce5ee13829b0d77855f49d6865d
Nico Muszalczyk <74101214+starsbit@users.noreply.github.com>
2025-02-06 21:45:03 +0100

    add analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './animations/route-animations';
 import { BackgroundComponent } from './components/background/background.component';
+import { GoogleAnalyticsTagComponent } from './components/google-analytics-tag/google-analytics-tag.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, NavigationComponent, BackgroundComponent],
+  imports: [
+    RouterOutlet,
+    NavigationComponent,
+    BackgroundComponent,
+    GoogleAnalyticsTagComponent,
+  ],
   animations: [slideInAnimation],
 })
 export class AppComponent {}
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  googleAnalyticsTrackingCode: 'G-8QWBE016HK',
+};
