c2940477e157be2d613c0e9a409484722c79390a
Nico Muszalczyk <74101214+starsbit@users.noreply.github.com>
2025-02-06 16:33:34 +0100

    add ssr deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), provideClientHydration(withEventReplay()),
   ],
 };
--- tools/copy-files.mjs
+++ tools/copy-files.mjs
@@ -1,14 +1,15 @@
-import fs from 'node:fs';
-import { join } from 'node:path';
-import { worker } from './paths.mjs';
+// Copy the files over so that they can be uploaded by the pages publish command.
+import fs from "node:fs";
+import { join, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-const sourceDir = join(worker, 'dist', 'browser');
-const destDir = join(worker, 'dist', 'bardle');
+const root = resolve(fileURLToPath(import.meta.url), "../../");
+const client = resolve(root, "dist/browser");
+const ssr = resolve(root, "dist/server");
+const cloudflare = resolve(root, "dist/cloudflare");
+const worker = resolve(cloudflare, "_worker.js");
 
-// Ensure the destination directory exists
-if (!fs.existsSync(destDir)) {
-  fs.mkdirSync(destDir, { recursive: true });
-}
+fs.cpSync(client, cloudflare, { recursive: true });
+fs.cpSync(ssr, worker, { recursive: true });
 
-// Copy files from source to destination
-fs.cpSync(sourceDir, destDir, { recursive: true });
+fs.renameSync(join(worker, "server.mjs"), join(worker, "index.js"));
