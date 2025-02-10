347c35557c191e037bb374f14f4fe0f15a5085ad
Nico Muszalczyk <74101214+starsbit@users.noreply.github.com>
2025-02-10 09:27:52 +0100

    json operation refactored into json_utils

--- scripts/filter_existing_students.py
+++ scripts/filter_existing_students.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from folder_management import get_asset_folder
+from json_utils import load_jp_data
 from schaledb_utils import translate_schale_to_wiki
 
 
@@ -11,10 +12,9 @@ def filter_existing_students(students):
     if not os.path.exists(character_info_file):
         return students
     
-    with open(character_info_file, 'r') as file:
-        character_info = json.load(file)
-        # the keys are different here from what we get. The spaces are replaced with underscores
-        character_info = [key.replace("_", " ") for key in character_info.keys()]
+    character_info = load_jp_data()
+    # the keys are different here from what we get. The spaces are replaced with underscores
+    character_info = [key.replace("_", " ") for key in character_info.keys()]
 
     filtered_students = [student for student in students if translate_schale_to_wiki(student) not in character_info]
     print("Filtered students:", filtered_students, character_info)
