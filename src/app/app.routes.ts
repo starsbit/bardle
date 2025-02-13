8da890ea802815d6c5a4c454eaea048507586cf1
Nico Muszalczyk <74101214+starsbit@users.noreply.github.com>
2025-02-13 21:39:16 +0100

    update routing and asset fetching

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,36 +2,31 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: ':locale',
-    children: [
-      {
-        path: '',
-        loadComponent: () =>
-          import('./pages/game/game.component').then((m) => m.GameComponent),
-        data: { animation: 'HomePage' },
-        title: $localize`:Bardle|Bardle Page Name@@BardlePageName:Bardle`,
-      },
-      {
-        path: 'about',
-        loadComponent: () =>
-          import('./pages/about/about.component').then((m) => m.AboutComponent),
-        data: { animation: 'AboutPage' },
-        title: $localize`:Bardle - About|Bardle - About Page Name@@BardleAboutPageName:Bardle - About`,
-      },
-      {
-        path: 'contact',
-        loadComponent: () =>
-          import('./pages/contact/contact.component').then(
-            (m) => m.ContactComponent
-          ),
-        data: { animation: 'ContactPage' },
-        title: $localize`:Bardle - Contact|Bardle - Contact Page Name@@BardleContactPageName:Bardle - Contact`,
-      },
-      {
-        path: '**',
-        redirectTo: '',
-        pathMatch: 'full',
-      },
-    ],
+    path: '',
+    loadComponent: () =>
+      import('./pages/game/game.component').then((m) => m.GameComponent),
+    data: { animation: 'HomePage' },
+    title: $localize`:Bardle|Bardle Page Name@@BardlePageName:Bardle`,
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./pages/about/about.component').then((m) => m.AboutComponent),
+    data: { animation: 'AboutPage' },
+    title: $localize`:Bardle - About|Bardle - About Page Name@@BardleAboutPageName:Bardle - About`,
+  },
+  {
+    path: 'contact',
+    loadComponent: () =>
+      import('./pages/contact/contact.component').then(
+        (m) => m.ContactComponent
+      ),
+    data: { animation: 'ContactPage' },
+    title: $localize`:Bardle - Contact|Bardle - Contact Page Name@@BardleContactPageName:Bardle - Contact`,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
   },
 ];
