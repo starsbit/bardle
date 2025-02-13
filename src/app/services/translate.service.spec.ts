3d71cb114f38ac6cc123d848bf73d06902d60a6d
Nico Muszalczyk <74101214+starsbit@users.noreply.github.com>
2025-02-13 22:39:09 +0100

    fix translate service tests

--- src/app/services/translate.service.spec.ts
+++ src/app/services/translate.service.spec.ts
@@ -29,7 +29,7 @@ describe('TranslateService', () => {
     expect(availableLanguages).toContain({ code: 'en', name: 'English' });
     expect(availableLanguages).toContain({ code: 'ja', name: 'Japanese' });
     expect(availableLanguages).toContain({
-      code: 'zh-Hans',
+      code: 'zh',
       name: 'Chinese (Simplified)',
     });
     expect(availableLanguages).toContain({ code: 'ko', name: 'Korean' });
@@ -60,7 +60,7 @@ describe('TranslateService', () => {
       ],
     });
     service = TestBed.inject(TranslateService);
-    expect(service.getCurrentLang().code).toBe('zh-Hans');
+    expect(service.getCurrentLang().code).toBe('zh');
 
     TestBed.resetTestingModule();
     TestBed.configureTestingModule({
